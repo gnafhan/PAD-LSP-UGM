8833972f11622235b048be73c96d5d3302cf9ad4
annisa-ugm <annisamutiarahman@mail.ugm.ac.id>
2024-09-07 17:58:08 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/about', function () {
+//     return view('contents.about', [
+//         "name" => "lala",
+//         "email" => "lala@gmail.com"
+//     ]);
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('contents.dashboard');
+// });
+
+// Route::get('/headerFooter', function () {
+//     return view('folderLayout.headerFooter');
+// });
+
+// Route::get('/layout', function () {
+//     return view('folderLayout.layout');
+// });
+
+// Route::get('/about', function () {
+//     return view('contents.about');
+// })->name('about');
+
+// P3
+// Route::get('/posts', function () {
+//     return view('posts');
+// });
+
+Route::get('/posts', [PostController::class, 'index']);
