ae49a32b5d6a216170a591b8e522e53fb0ebf45f
bellputrii <beldaputripramono@mail.ugm.ac.id>
2024-10-12 21:14:17 +0700

    Add landing page

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<header class="bg-blue-300 p-4 shadow-lg">
+        <div class="container mx-auto flex justify-between items-center">
+            <!-- Logo -->
+            <img src="{{ asset('images/logo.png') }}" alt="LSP UGM Logo" class="w-12">
+
+            <!-- Mobile Menu Button -->
+            <button id="menu-button" class="block lg:hidden text-white focus:outline-none">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+            </button>
+
+            <!-- Navbar Links -->
+            <nav id="navbar" class="hidden lg:flex space-x-4">
+                <a href="#home" class="text-white font-semibold hover:text-gray-200">Home</a>
+                <a href="#guide" class="text-white font-semibold hover:text-gray-200">Panduan</a>
+                <a href="#profile" class="text-white font-semibold hover:text-gray-200">Profil</a>
+                <a href="/masuk" class="text-white font-semibold hover:text-gray-200">Masuk/Daftar</a>
+            </nav>
+        </div>
+    </header>
+
+    <script>
+        // JavaScript to toggle the navbar on small screens
+        const menuButton = document.getElementById('menu-button');
+        const navbar = document.getElementById('navbar');
+
+        menuButton.addEventListener('click', () => {
+            navbar.classList.toggle('hidden');
+        });
+    </script>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,40 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/masuk', function () {
+    return view('masuk');
+});
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Opsi Login Pertama
+Route::get('/loginasesi', function () {
+    return view('loginasesi');
+});
+Route::get('/loginasesor', function () {
+    return view('loginasesor');
+});
+Route::get('/loginadmin', function () {
+    return view('loginadmin');
+});
+
+// Opsi login kedua
+// asesi tetep make opsi pertama
+Route::get('/asesor', function () {
+    return view('loginasesor');
+});
+Route::get('/admin', function () {
+    return view('loginadmin');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/resetpw', function () {
+    return view('resetpw');
+});
+Route::get('/forgetpw', function () {
+    return view('forgetpw');
+});
