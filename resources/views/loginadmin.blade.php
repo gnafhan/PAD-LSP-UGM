5592e6ca0115614492f7fbc0073466b3ccf9150d
bellputrii <beldaputripramono@mail.ugm.ac.id>
2024-10-13 08:50:33 +0700

    Add field access code in admin and asesor

--- resources/views/loginadmin.blade.php
+++ resources/views/loginadmin.blade.php
@@ -39,6 +39,13 @@
                 class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
         </div>
 
+        <!-- Access Code -->
+        <div class="mb-4">
+            <label for="access-code" class="block text-sm font-medium text-gray-700 mb-2">Access Code</label>
+            <input type="access-code" id="access-code" placeholder="Enter Access Code..."
+                class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
+        </div>
+
         <!-- Remember Me and Forgot Password -->
         <div class="flex items-center justify-between mb-6">
             <div class="flex items-center">
