f93220e6d55a379f82d12d9b97e27112a3bd251c
rizwanhamka <ahmadrizwanhamka@mail.ugm.ac.id>
2025-08-14 19:17:27 +0700

    frak03 asesor done

--- resources/views/home/home-asesor/frak03-asesor.blade.php
+++ resources/views/home/home-asesor/frak03-asesor.blade.php
@@ -186,19 +186,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 <tr>
                     <td class="px-4 py-3 text-sm text-gray-700">${index + 1}</td>
                     <td class="px-4 py-3 text-center">
-                        <a href="${detailUrl}" class="mr-2">
-                            <svg class="w-6 h-6 text-biru hover:text-ungu" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd"
-                                    d="M21.707 21.707a1 1 0 0 1-1.414 0l-3.5-3.5a1 1 0 0 1 1.414-1.414l3.5 3.5a1 1 0 0 1 0 1.414ZM2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm9-3a1 1 0 1 0-2 0v2H7a1 1 0 0 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0-2h-2V7Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                        <a href="${detailUrl}" class="mr-2">
-                            <svg class="w-6 h-6 text-ungu hover:text-biru" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M8 3a2 2 0 0 0-2 2v3h12V5a2 2 0 0 0-2-2H8Zm-3 7a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h1v-4a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v4h1a2 2 0 0 0 2-2v-5a2 2 0 0 0-2-2H5Zm4 11a1 1 0 0 1-1-1v-4h8v4a1 1 0 0 1-1 1H9Z" clip-rule="evenodd"/>
-                            </svg>
-                        </a>
+                        <div class="flex justify-center items-center gap-2">
+                            <a href="${detailUrl}">
+                                <svg class="w-6 h-6 text-biru hover:text-ungu" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M21.707 21.707a1 1 0 0 1-1.414 0l-3.5-3.5a1 1 0 0 1 1.414-1.414l3.5 3.5a1 1 0 0 1 0 1.414ZM2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm9-3a1 1 0 1 0-2 0v2H7a1 1 0 0 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0-2h-2V7Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                            <a href="${detailUrl}">
+                                <svg class="w-6 h-6 text-ungu hover:text-biru" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M8 3a2 2 0 0 0-2 2v3h12V5a2 2 0 0 0-2-2H8Zm-3 7a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h1v-4a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v4h1a2 2 0 0 0 2-2v-5a2 2 0 0 0-2-2H5Zm4 11a1 1 0 0 1-1-1v-4h8v4a1 1 0 0 1-1 1H9Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        </div>
                     </td>
+
                     <td class="px-4 py-3 text-gray-700 text-left">${asesi.nama_asesi}</td>
                     <td class="px-4 py-3 text-gray-700 text-left">${asesi.nama_skema}</td>
                     <td class="px-4 py-3 text-gray-700 text-left">${asesi.nomor_skema}</td>
