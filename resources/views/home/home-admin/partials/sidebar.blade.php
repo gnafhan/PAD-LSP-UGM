7d2d3784743470c07035b4e9d81821abead4313d
gnafhan <nafhanghifari@gmail.com>
2025-12-22 03:28:54 +0700

    feat: add sidebar

--- resources/views/home/home-admin/partials/sidebar.blade.php
+++ resources/views/home/home-admin/partials/sidebar.blade.php
@@ -45,12 +45,12 @@
 
 
         {{-- SOALLLL --}}
-        <a href="{{ route('admin.soal.index') }}" class="admin-sidebar-link {{ request()->routeIs('admin.soal.*') ? 'admin-sidebar-link-active' : '' }}" :class="collapsed ? 'justify-center' : ''" title="Skema">
+        {{-- <a href="{{ route('admin.soal.index') }}" class="admin-sidebar-link {{ request()->routeIs('admin.soal.*') ? 'admin-sidebar-link-active' : '' }}" :class="collapsed ? 'justify-center' : ''" title="Skema">
         <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
         </svg>
             <span class="ml-3 transition-all duration-300" x-show="!collapsed" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform -translate-x-2" x-transition:enter-end="opacity-100 transform translate-x-0" x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100 transform translate-x-0" x-transition:leave-end="opacity-0 transform -translate-x-2">Manajemen Soal</span>
-        </a>
+        </a> --}}
         {{-- SOALLLLL --}}
 
 
