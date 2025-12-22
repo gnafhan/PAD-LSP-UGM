27b4f4bea013f0f0f7769d0693c6526318bc1f87
gnafhan <nafhanghifari@gmail.com>
2025-12-22 15:01:53 +0700

    fix bug visual

--- resources/views/home/home-admin/pengguna.blade.php
+++ resources/views/home/home-admin/pengguna.blade.php
@@ -842,19 +842,19 @@
 
                                             <!-- Tombol Edit Hasil Asesmen -->
                                             @if($asesi->hasil_asesmen)
-                                                <a href="{{ route('admin.hasil-asesmen.edit', $asesi->hasil_asesmen->id) }}" class="inline-flex items-center px-3 py-1.5 bg-blue-600 hover:bg-blue-700 rounded-md text-white transition-all">
+                                                {{-- <a href="{{ route('admin.hasil-asesmen.edit', $asesi->hasil_asesmen->id) }}" class="inline-flex items-center px-3 py-1.5 bg-blue-600 hover:bg-blue-700 rounded-md text-white transition-all">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                                     </svg>
                                                     Edit Hasil
-                                                </a>
+                                                </a> --}}
                                             @elseif($asesi->rincianAsesmen)
-                                                <a href="{{ route('admin.hasil-asesmen.create', $asesi->rincianAsesmen->id_rincian_asesmen) }}" class="inline-flex items-center px-3 py-1.5 bg-green-600 hover:bg-green-700 rounded-md text-white transition-all">
+                                                {{-- <a href="{{ route('admin.hasil-asesmen.create', $asesi->rincianAsesmen->id_rincian_asesmen) }}" class="inline-flex items-center px-3 py-1.5 bg-green-600 hover:bg-green-700 rounded-md text-white transition-all">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                                                     </svg>
                                                     Buat Hasil
-                                                </a>
+                                                </a> --}}
                                             @endif
 
                                             @if($asesi->hasil_asesmen && $asesi->hasil_asesmen->status === 'kompeten')
