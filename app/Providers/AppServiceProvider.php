2c5584ffb76321e281083cdf0df4e6a9a4c463f6
gnafhan <nafhanghifari@gmail.com>
2026-01-29 14:23:46 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS for staging and production environments
+        if (app()->environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
